30905330e04d2d5a4e0fd1ce49eab99b2546d139
raghvsin <136577375+raghvsin@users.noreply.github.com>
2023-08-29 20:47:47 +0530

    update

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
-var btnTranslate= document.querySelector("#btn-translate")
+var btnTranslate=document.querySelector("#btn-translate");
+var txtInput=document.querySelector("#txt-input");
+var outputDiv=document.querySelector("#output");
+var serverURL="https://api.funtranslations.com/translate/minion.json"
 
-console.log(btnTranslate)
+function getTranslationURL(input){
+    return serverURL+"?"+"text="+input
+}
+
+function errorHandler(error){
+    console.log("Error Occured",error)
+}
+
+function clickHandler(){
+
+    var inputText=txtInput.value;//taking input
+
+    //calling server for processing
+    fetch(getTranslationURL(inputText))
+    .then(response => response.json())
+    .then(json=> {
+        var translatedText=json.contents.translated;
+        outputDiv.innerText=translatedText;//output
+    })
+    .catch(errorHandler)
+};
+btnTranslate.addEventListener("click",clickHandler)
